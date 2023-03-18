e6eb6da2922b84913c83fceedc0b41bac2b43c0f
souraOP <sourasishmondal95@gmail.com>
2023-03-18 17:09:15 +0530

    Adding the website folder

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+    "use strict";
+
+    //Preloader
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 100, 'easeInOutExpo');
+        return false;
+    });
+
+
+    
+})(jQuery);
+
